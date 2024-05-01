866bf8c79928493414b9db29ada5acbc0b94fb85
Felipe Aires Villaça <felipevillaca27@gmail.com>
2024-05-01 17:05:07 -0300

    Atualização quarto + criando main

--- Quarto.java
+++ Quarto.java
@@ -4,4 +4,42 @@ public class Quarto {
     private boolean ocupado;
     private boolean chaveNaRecepcao;
     
+    public Quarto(int numero, int capacidade) {
+        this.numero = numero;
+        this.capacidade = capacidade;
+        this.ocupado = false;
+        this.chaveNaRecepcao = true; // Inicialmente a chave está na recepção
+    }
+    
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    public boolean isChaveNaRecepcao() {
+        return chaveNaRecepcao;
+    }
+
+    public void setChaveNaRecepcao(boolean chaveNaRecepcao) {
+        this.chaveNaRecepcao = chaveNaRecepcao;
+    }
 }
